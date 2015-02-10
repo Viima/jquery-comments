d4c59b5a731248bf1fb47004a832530c44a2f0ba
Joona Tykkyläinen <joona@viima.com>
2015-02-10 13:03:02 +0200

    Add the comment to the data model after it's created

--- js/jquery-comments.js
+++ js/jquery-comments.js
@@ -74,8 +74,7 @@
             // Create CSS declarations for highlight color
             this.createCssDeclarations();
 
-            this.updateData();
-            this.render();
+            this.refresh();
         },
 
         delegateEvents: function() {
@@ -103,7 +102,14 @@
         // Basic functionalities
         // =====================
 
+        refresh: function() {
+            this.updateData();
+            this.render();
+        },
+
         updateData: function () {
+            this.commentsById = {};
+
             // Get comments
             var commentsArray = this.options.getComments();
 
@@ -185,7 +191,6 @@
         },
 
         addComment: function(commentModel, rearrange) {
-            this.addCommentToDataModel(commentModel);
             var commentEl = this.createCommentElement(commentModel);
 
             // Case: reply
@@ -401,7 +406,8 @@
                     content: this.getTextareaContent(textarea)
                 }
                 var commentModel = this.createCommentModel(data);
-
+                this.addCommentToDataModel(commentModel);
+                
                 this.postComment(commentModel);
                 this.addComment(commentModel, true);
 
