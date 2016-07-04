a134f18a80f2a640f9baf5a951cbc6404a929524
Joona Tykkyläinen <joona@viima.com>
2016-07-04 13:37:41 +0300

    Updated the version

--- js/jquery-comments.js
+++ js/jquery-comments.js
@@ -1,4 +1,4 @@
-//     jquery-comments.js 1.1.1
+//     jquery-comments.js 1.1.2
 
 //     (c) 2016 Joona Tykkyläinen, Viima Solutions Oy
 //     jquery-comments may be freely distributed under the MIT license.
