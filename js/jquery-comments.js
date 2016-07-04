e57282d50cb0ec5dd2304b41200a2057e8f28009
Joona Tykkyläinen <joona@viima.com>
2016-07-04 12:51:56 +0300

    Moved default options to separate function

--- js/jquery-comments.js
+++ js/jquery-comments.js
@@ -97,100 +97,102 @@
         },
 
 
+        // Default options
+        // ===============
+
+        getDefaultOptions: function() {
+            return {
+
+                // User        
+                profilePictureURL: '',        
+                currentUserIsAdmin: false,        
+                
+                // Font awesome icon overrides        
+                spinnerIconURL: '',       
+                upvoteIconURL: '',        
+                replyIconURL: '',     
+                uploadIconURL: '',        
+                attachmentIconURL: '',        
+                fileIconURL: '',      
+                noCommentsIconURL: '',        
+                
+                // Strings to be formatted (for example localization)     
+                textareaPlaceholderText: 'Add a comment',     
+                newestText: 'Newest',     
+                oldestText: 'Oldest',     
+                popularText: 'Popular',       
+                attachmentsText: 'Attachments',       
+                sendText: 'Send',     
+                replyText: 'Reply',       
+                editText: 'Edit',     
+                editedText: 'Edited',     
+                youText: 'You',       
+                saveText: 'Save',     
+                deleteText: 'Delete',     
+                viewAllRepliesText: 'View all __replyCount__ replies',        
+                hideRepliesText: 'Hide replies',      
+                noCommentsText: 'No comments',        
+                noAttachmentsText: 'No attachments',      
+                attachmentDropText: 'Drop files here',        
+                textFormatter: function(text) {return text},        
+                
+                // Functionalities        
+                enableReplying: true,     
+                enableEditing: true,      
+                enableUpvoting: true,     
+                enableDeleting: true,     
+                enableAttachments: false,     
+                enableDeletingCommentWithReplies: false,      
+                enableNavigation: true,       
+                postCommentOnEnter: false,        
+                forceResponsive: false,       
+                readOnly: false,      
+                defaultNavigationSortKey: 'newest',       
+                
+                // Colors     
+                highlightColor: '#2793e6',        
+                deleteButtonColor: '#C9302C',     
+                
+                roundProfilePictures: false,      
+                textareaRows: 2,      
+                textareaRowsOnFocus: 2,       
+                textareaMaxRows: 5,       
+                maxRepliesVisible: 2,     
+                
+                fieldMappings: {      
+                    id: 'id',     
+                    parent: 'parent',     
+                    created: 'created',       
+                    modified: 'modified',     
+                    content: 'content',       
+                    file: 'file',     
+                    fileURL: 'file_url',      
+                    fileMimeType: 'file_mime_type',       
+                    fullname: 'fullname',     
+                    profileURL: 'profile_url',        
+                    profilePictureURL: 'profile_picture_url',     
+                    createdByAdmin: 'created_by_admin',       
+                    createdByCurrentUser: 'created_by_current_user',      
+                    upvoteCount: 'upvote_count',      
+                    userHasUpvoted: 'user_has_upvoted'        
+                },        
+                
+                getComments: function(success, error) {success([])},      
+                postComment: function(commentJSON, success, error) {success(commentJSON)},        
+                putComment: function(commentJSON, success, error) {success(commentJSON)},     
+                deleteComment: function(commentJSON, success, error) {success()},     
+                upvoteComment: function(commentJSON, success, error) {success(commentJSON)},      
+                uploadAttachments: function(commentArray, success, error) {success(commentArray)},        
+                refresh: function() {},       
+                timeFormatter: function(time) {return new Date(time).toLocaleDateString()}
+            }
+        },
+
+
         // Initialization
         // ==============
 
         init: function(options, el) {
-        	var defaults = {
-			  // User
-			  profilePictureURL: '',
-			  currentUserIsAdmin: false,
-	
-			  // Font awesome icon overrides
-			  spinnerIconURL: '',
-			  upvoteIconURL: '',
-			  replyIconURL: '',
-			  uploadIconURL: '',
-			  attachmentIconURL: '',
-			  fileIconURL: '',
-			  noCommentsIconURL: '',
-	
-			  // Strings to be formatted (for example localization)
-			  textareaPlaceholderText: 'Add a comment',
-			  newestText: 'Newest',
-			  oldestText: 'Oldest',
-			  popularText: 'Popular',
-			  attachmentsText: 'Attachments',
-			  sendText: 'Send',
-			  replyText: 'Reply',
-			  editText: 'Edit',
-			  editedText: 'Edited',
-			  youText: 'You',
-			  saveText: 'Save',
-			  deleteText: 'Delete',
-			  viewAllRepliesText: 'View all __replyCount__ replies',
-			  hideRepliesText: 'Hide replies',
-			  noCommentsText: 'No comments',
-			  noAttachmentsText: 'No attachments',
-			  attachmentDropText: 'Drop files here',
-			  textFormatter: function(text) {
-				 return text;
-			  },
-	
-			  // Functionalities
-			  enableReplying: true,
-			  enableEditing: true,
-			  enableUpvoting: true,
-			  enableDeleting: true,
-			  enableAttachments: false,
-			  enableDeletingCommentWithReplies: false,
-			  enableNavigation: true,
-			  postCommentOnEnter: false,
-			  forceResponsive: false,
-			  readOnly: false,
-			  defaultNavigationSortKey: 'newest',
-	
-			  // Colors
-			  highlightColor: '#2793e6',
-			  deleteButtonColor: '#C9302C',
-	
-			  roundProfilePictures: false,
-			  textareaRows: 2,
-			  textareaRowsOnFocus: 2,
-			  textareaMaxRows: 5,
-			  maxRepliesVisible: 2,
-	
-			  fieldMappings: {
-				 id: 'id',
-				 parent: 'parent',
-				 created: 'created',
-				 modified: 'modified',
-				 content: 'content',
-				 file: 'file',
-				 fileURL: 'file_url',
-				 fileMimeType: 'file_mime_type',
-				 fullname: 'fullname',
-				 profileURL: 'profile_url',
-				 profilePictureURL: 'profile_picture_url',
-				 createdByAdmin: 'created_by_admin',
-				 createdByCurrentUser: 'created_by_current_user',
-				 upvoteCount: 'upvote_count',
-				 userHasUpvoted: 'user_has_upvoted'
-			  },
-	
-			  getComments: function(success, error) {success([])},
-			  postComment: function(commentJSON, success, error) {success(commentJSON)},
-			  putComment: function(commentJSON, success, error) {success(commentJSON)},
-			  deleteComment: function(commentJSON, success, error) {success()},
-			  upvoteComment: function(commentJSON, success, error) {success(commentJSON)},
-			  uploadAttachments: function(commentArray, success, error) {success(commentArray)},
-			  refresh: function() {},
-			  timeFormatter: function(time) {
-				 return new Date(time).toLocaleDateString();
-			  }
-        
-        	};
-        
             this.$el = $(el);
             this.$el.addClass('jquery-comments');
             this.undelegateEvents();
@@ -201,8 +203,7 @@
             if($.browser.mobile) this.$el.addClass('mobile');
 
             // Init options
-            var settings = $.extend(true, {}, defaults, options);
-            this.options = settings;
+            this.options = $.extend(true, {}, this.getDefaultOptions(), options);;
 
             // Read-only mode
             if(this.options.readOnly) this.$el.addClass('read-only');
