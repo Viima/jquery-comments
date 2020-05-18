9f22d6d3dc6bf25fa352e735089008c6a79bad67
Joona Tykkylainen <joona@viima.com>
2020-05-18 13:51:16 +0300

    DEV-649: Minor optimization

--- js/jquery-comments.js
+++ js/jquery-comments.js
@@ -366,7 +366,7 @@
 
             // Create comments and attachments
             this.createComments();
-            this.createAttachments();
+            if(this.options.enableAttachments) this.createAttachments();
 
             // Remove spinner
             this.$el.find('> .spinner').remove();
